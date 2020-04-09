fd60380c4a84f49da5a71825ae486fa95d2a6905
Hayden Nesbit <hayden.nesbit@campusoutreach.org>
2020-04-09 16:21:58 -0400

    Updates

---
+++ precache-manifest.6c60e3c1666ca28160002dec2c3feab8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ac3a5a50e3b62b92ee15e4c77a563d",
+    "url": "/react-puzzle/index.html"
+  },
+  {
+    "revision": "568aca5e31511a4158db",
+    "url": "/react-puzzle/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b643f2785e209c8ba2a6",
+    "url": "/react-puzzle/static/css/main.4605269c.chunk.css"
+  },
+  {
+    "revision": "568aca5e31511a4158db",
+    "url": "/react-puzzle/static/js/2.836d4ae0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-puzzle/static/js/2.836d4ae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b643f2785e209c8ba2a6",
+    "url": "/react-puzzle/static/js/main.1143c91b.chunk.js"
+  },
+  {
+    "revision": "3811972c59187510dec3",
+    "url": "/react-puzzle/static/js/runtime-main.5869cf2f.js"
+  }
+]);
